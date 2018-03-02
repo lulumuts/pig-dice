752a614279654fc108ab5b44e39c9f5d1da8f259
Lulu <lulumutuli24@gmail.com>
2018-03-02 17:51:45 +0300

    dice total adding up

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 var total = 0;
 function rollDice() {
   var roll = Math.floor(Math.random() * 6) + 1;
-  $("#die1").append(roll);
-  $("#status").append(roll+total);
+  $("#die1").text(roll);
+  $("#status").text(roll+total);
 
 console.log(roll);
 }
 
 function rollDice2(){
   var roll2 = Math.floor(Math.random() * 6) + 1;
-  $("#die2").append(roll2);
+
+  $("#die2").text(roll2);
   console.log(roll2);
 }
   // for (var i=1; i<=6; i++) {
   //   if (i === 1) {
-
-
-
-  
 
 
 $(document).ready(function() {
@@ -32,3 +29,19 @@ $(document).ready(function() {
 
 });
 });
+
+// function rollDice(){
+//   var total = 0;
+// var roll = Math.floor(Math.random() * 6) + 1;
+// if(roll != 1){
+//   total =total + roll;
+// }
+// return total;
+// }
+// $(document).ready(function(){
+// $("#player1roll").click(function(e){
+//   e.preventDefault();
+//   // console.log("andrew");
+//   $("#die1").text(rollDice());
+// });
+// });
