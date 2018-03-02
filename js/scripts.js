61c83a007dde0a61efdfd35a67a9bef4de7dd65d
Lulu <lulumutuli24@gmail.com>
2018-03-02 16:49:53 +0300

    dice two initial javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
-
+var total = 0;
 function rollDice() {
   var roll = Math.floor(Math.random() * 6) + 1;
-  var total = document.getElementById('status').value;
   $("#die1").append(roll);
+  $("#status").append(roll+total);
   console.log(roll);
-
 }
+function rollDice2() {
+var roll2 = Math.floor(Math.random() * 6) + 1;
+$("#die2").append(roll2);
+  // for (var i=1; i<=6; i++) {
+  //   if (i === 1) {
+
+  console.log(roll2);
+  }
+
 
 $(document).ready(function() {
   $("#player1roll").click(function(event){
     event.preventDefault();
-
     rollDice();
-
-  })
+  $("#player2roll").click(function(event){
+    event.preventDefault();
+    rollDice2();
+    });
+  });
 });
