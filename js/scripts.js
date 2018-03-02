b4ee7941244b85f586ee8bbac32ef1a648eedccc
Lulu <lulumutuli24@gmail.com>
2018-03-02 17:19:23 +0300

    player 2 button initial javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,32 @@ function rollDice() {
   var roll = Math.floor(Math.random() * 6) + 1;
   $("#die1").append(roll);
   $("#status").append(roll+total);
-  console.log(roll);
+
+console.log(roll);
 }
-function rollDice2() {
-var roll2 = Math.floor(Math.random() * 6) + 1;
-$("#die2").append(roll2);
+
+function rollDice2(){
+  var roll2 = Math.floor(Math.random() * 6) + 1;
+  $("#die2").append(roll2);
+  console.log(roll2);
+}
   // for (var i=1; i<=6; i++) {
   //   if (i === 1) {
 
-  console.log(roll2);
-  }
+
+
+  
 
 
 $(document).ready(function() {
   $("#player1roll").click(function(event){
     event.preventDefault();
     rollDice();
-  $("#player2roll").click(function(event){
+  $("#player2roll").click(function(event) {
     event.preventDefault();
-    rollDice2();
-    });
-  });
+    rollDice2()
+  })
+
+
+});
 });
