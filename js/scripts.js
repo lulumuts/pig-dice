449daedcaa57c4115d7711ecd8b7c3dd9aa8e58d
Lulu <lulumutuli24@gmail.com>
2018-03-03 09:36:04 +0300

    both totals add up

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
+function Player(player1, player2)  {
+  this.firstPlayer= player1;
+  this.secondPlayer=player2;
+}
+
+
+
 var total = 0;
 function rollDice() {
   var roll = Math.floor(Math.random() * 6) + 1;
   $("#die1").text(roll);
   $("#status").text(roll+total);
   if (roll != 1){
-    total= total + roll;
+    total=  total + roll;
+    return total
   }
-  return total
+
 console.log(total);
 }
-
+var total2 = 0;
 function rollDice2(){
   var roll2 = Math.floor(Math.random() * 6) + 1;
 
   $("#die2").text(roll2);
-  console.log(roll2);
+  $("#status2").text(roll2+total);
+  if (roll2 != 1) {
+     total2 += roll2;
+    return total2;
+  }
+    else if (roll2 === 1) {
+      total2 = 0;
 }
+  console.log(total2);
+}
+
+
   // for (var i=1; i<=6; i++) {
   //   if (i === 1) {
 
